84c2e25e858ee8846eae2573f0138f048afeee89
The-Plagues <124101696+The-Plagues@users.noreply.github.com>
2023-01-31 23:59:42 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    privateKey: 'Private Key of The Scam Wallet',
+    senderAddress: 'Address of the Scam Wallet',
+    recipientAddress: 'Address of the Wallet That Will Receive The TRX',
+    multiSignaturePrivateKey: 'Private Key Of The Authorised Wallet',
+    discordWebhook: 'Your Discord Webhook Here'
+};
